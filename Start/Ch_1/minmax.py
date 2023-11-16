05ecf18aa21b5a39aeeed1102e518db7c3bd5d5d
Patryk Prezyna <patryk.prezyna@rommelag.com>
2023-11-16 20:02:44 +0100

    learned about the key function nad built in data structure functions2

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,14 +9,24 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
+print(f'min: {min(values)}', )
 
 # TODO: The max() function finds the maximum value
 
 
 # TODO: define a custom "key" function to extract a data field
-
+print(f'Min: {min(strings, key=len)}')
 
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+
+def functionx(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if magnitude is None:
+        magnitude = 0
+    return float(magnitude)
+
+with open("30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+    print(f'{data["metadata"]["title"]}')
+    print(f'min: {min(data["features"], key=functionx)}')
+
